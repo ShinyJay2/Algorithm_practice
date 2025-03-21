8ca0ad25022d432a0dfca34d3c4cfdccc34f3c51
Jaehoon Jang <64793331+ShinyJay2@users.noreply.github.com>
2025-03-22 02:17:05 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250322/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -0,0 +1,21 @@
+n = int(input())  #숫자 배열의 길이
+arr = list(map(int, input().split()))  #실제 배열
+
+# "N개의 원소 중 최댓값을 구하는 함수" 가 우리의 재귀함수이다
+
+def get_maximum(n):
+
+    tmp = []
+
+    if n == 1:
+        return n
+
+    for i in range(n):
+        tmp.append(arr[i])
+
+    MAX = max(tmp)
+
+    return max(get_maximum(n-1), MAX)
+
+
+print(get_maximum(n))
