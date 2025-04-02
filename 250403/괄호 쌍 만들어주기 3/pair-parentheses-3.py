e17c333a7535b84d30f03e4d94e828e9c5d5ae09
Jaehoon Jang <64793331+ShinyJay2@users.noreply.github.com>
2025-04-03 01:26:49 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250403/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,13 @@
+A = input()
+
+count = 0
+
+for i in range(len(A)):
+
+    if A[i] == '(':
+
+        for j in range(i+1, len(A)):
+            if A[j] == ')':
+                count += 1
+
+print(count)
