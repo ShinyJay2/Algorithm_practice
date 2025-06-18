1b5e7137f118679f4fbb39bfb1fb02682ce050c6
Jaehoon Jang <64793331+ShinyJay2@users.noreply.github.com>
2025-06-19 04:49:37 +0900

    Title: N개 중에 M개 뽑기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250619/N개 중에 M개 뽑기/n-choose-m.py
+++ 250619/N개 중에 M개 뽑기/n-choose-m.py
@@ -1,13 +1,28 @@
 N, M = map(int, input().split())
 
-from itertools import combinations
-
 num_list = [i for i in range(1, N+1)]
 
-result = []
 
-for comb in combinations(num_list, M):
-    result.append(comb)
-    print(comb[0], comb[1])
+ans = []
+
+# We need a dfs, maybe with a without used, leftover nums
+
+def dfs(idx, choice_start):
+
+    global ans # The case where we modify global structure, so need of backtracking implementation
+
+    if idx == M:
+        print(*ans)
+        return
+
+    # We need to decide the first, then call the next
+    # The deciding number range is n
+    for num in range(choice_start, N+1):
+        ans.append(num)
+        dfs(idx+1, num+1)
+        ans.pop()
+
+
+dfs(0, 1)
 
 
